c9c2939ac50319e9ae9889aad24e73a77ad972c5
armany87 <h.gonzalezrivera2@gmail.com>
2021-07-31 19:23:59 -0500

    Add files via upload

--- streamlit_template.py
+++ streamlit_template.py
@@ -121,8 +121,8 @@ with stats_container:
 
 
 	# 5 --- You can work with data, change it and filter it as you always do using Pandas or any other library
-	start_station_list = ['All'] + data['player id'].unique().tolist()
-	end_station_list = ['All'] + data['player team'].unique().tolist()
+		start_station_list = ['All'] + data['start station name'].unique().tolist()
+	end_station_list = ['All'] + data['end station name'].unique().tolist()
 
 
 
